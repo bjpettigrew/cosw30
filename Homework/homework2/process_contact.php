0a0d4f8f704f0295f4f6dfb92907099c70c12175
EC2 Default User <ec2-user@ip-172-31-92-151.ec2.internal>
2019-09-10 06:51:27 +0000

    Revise response code again v 2.0

--- Homework/homework2/process_contact.php
+++ Homework/homework2/process_contact.php
@@ -36,14 +36,14 @@
     
    if ( !empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['age']))
    {
-        echo '<h1>Haley Joel Osment you are fantastic!  Jennifer Love Hewitt you\'ve signed up! <br> Thank you for supporting Just Jack! For President!</h1><br>';
-        echo '<h3>Hello $first_name $last_name, you identify $gender, your age range is $age, your political interests include $interests.  Your $donation contribution is appreciated.</h3> <br>';
-        echo '<h3>We appreciate your comments: <pre>$comments</pre>.  We will reply to you at email.</h3>';
+        echo "<h1>Haley Joel Osment you are fantastic!  Jennifer Love Hewitt you\'ve signed up! <br> Thank you for supporting Just Jack! For President!</h1><br>";
+        echo "<p>Hello $first_name $last_name, you identify $gender, your age range is $age, your political interests include $interests.</p><p>Your $donation contribution is appreciated.</p> <br>";
+        echo "<p>We appreciate your comments: </p> <pre>$comments</pre>.  <p>We will reply to you at email.</hp>";
     }
    else {
         //Error - output an error message
-        echo '<h1>There\'s no shame in getting it wrong.  There\'s only shame in getting ugly.  Please fill out the required fields and try again!</h1>';
-        echo '<a href="contact.html">Go back</a>'; 
+        echo "<h1>There\'s no shame in getting it wrong.  There\'s only shame in getting ugly.  Please fill out the required fields and try again!</h1>";
+        echo "<a href=\"contact.html\">Go back</a>"; 
     }
 ?>
 </body>
