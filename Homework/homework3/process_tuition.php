7ddb3d6ec34b4ffd5251de9de3cf082c58ed0154
EC2 Default User <ec2-user@ip-172-31-92-151.ec2.internal>
2019-09-15 22:56:41 +0000

    1st draft html file

---
+++ Homework/homework3/process_tuition.php
@@ -0,0 +1,52 @@
+
+
+
+
+<?php # Script 2.2 process_contact.php
+
+$resident = $_POST['resident'];
+$units = $_POST['units'];
+$mandatory = $_POST['mandatory'];
+$services = $_POST['services'];
+$parking = $_POST['parking'];
+$submit = $_POST[submit];
+
+
+if 'resident' =
+
+    echo "Cost of Tuition:  $" __ "unit(s) x $"  __ "= $" __
+
+else
+    
+    echo "Cost of Tuition:  $" __ "unit(s) x $"  __ "= $" __
+
+echo "Mandatory Student Health Fee: $" $mandatory
+
+if 'services' =
+
+echo "College Services Card: $" ___
+
+else
+
+echo "College Services Card: $" ___
+
+if 'parking' = 
+
+echo "Parking Permit: $" ___
+
+else
+
+echo "Parking Permit: $" ___
+
+echo "Total Registration Costs: $" ______
+
+echo "Scholarship Award: $"  ____
+
+echo "Total Balance Due At Registration: $" ___________
+
+
+
+
+
+
+?>
