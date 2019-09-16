f72c79e5fcd4d5f25909ca787059c4691942adc4
EC2 Default User <ec2-user@ip-172-31-92-151.ec2.internal>
2019-09-16 00:26:46 +0000

    add more php and style to php return

--- Homework/homework3/process_tuition.php
+++ Homework/homework3/process_tuition.php
@@ -1,4 +1,59 @@
-
+<!doctype html>
+<html>
+  <head>
+    <title>Tuition Calculator</title>
+    <meta charset="utf-8">
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Cinzel|Cormorant+Garamond&display=swap');
+      body {
+        background: black;
+        padding: 5%;
+      }
+      #container1 {
+        background: red;
+        padding: 5%;
+        border-style: ridge;
+      }
+      #form_box {
+        background: white;
+        padding: 5%;
+        text-align: center;
+        border-style: ridge;
+        }
+   
+      h1 {
+       font-family: 'Cinzel', serif; 
+      }
+      h2 {
+        font-family: 'Cormorant Garamond', serif;
+      }
+      form {
+        text-align:left;
+        padding: 2% 20%;
+      }
+      p {
+        font-family: 'Cormorant Garamond', serif;
+        font-size: 1.25em;
+        font-weight: bold;
+      }
+      #submit {
+        font-family: 'Cormorant Garamond', serif;
+        font-size: 1.25em;
+        font-weight: bold;
+        color: red;
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+        padding: 0 45%;
+      }
+      footer {
+        color: white;
+      }
+      
+    </style>
+  </head>
+  
+  <body>
+    <div id="container1">
+       <div id="form_box">  
 
 
 
@@ -14,22 +69,25 @@ $submit = $_POST[submit];
 $tuition = $resident * $units;
 $subtotal = $tuition + $mandatory + $services + $parking;
 //calculate random scholarship?
-//$scholarship = (rand (0, $total) . "<br>"');
+$scholarship = (rand(0, $tuition));
 $total = $subtotal - $scholarship;
 
 
-echo "Cost of Tuition: " .  $units . "unit(s) x $" . $resident . "cost per unit = $" . $tuition;
-
+echo "<div><h2> Cost of Tuition: " .  $units . "unit(s) x $" . $resident . "per unit = $" . $tuition . "</h2>";
+echo "<br>";
 echo "Mandatory Student Health Fee: $" . $mandatory;
-
+echo "<br>";
 echo "College Services Card: $" . $services;
-
+echo "<br>";
 echo "Parking Permit: $" . $parking;
-
+echo "<br>";
 echo "Total Registration Costs: $" . $subtotal;
+echo "<br></div><div>;
 
 
 
+
+<body>
 
 
 
