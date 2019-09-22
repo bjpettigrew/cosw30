c303d4195b841d599d92611d290653e35a40d602
EC2 Default User <ec2-user@ip-172-31-92-151.ec2.internal>
2019-09-22 22:31:31 +0000

    add PHP case-neatral sort?

--- Homework/homework4/quote_process.php
+++ Homework/homework4/quote_process.php
@@ -1,31 +1,53 @@
+<!doctype=html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Sorting Arrays</title>
+    </head>
+    <body>
+
+
 <?php
     
    
-    $text = $_POST['quote'];
-    $textExplode = explode(' ', $text);
+    $quote = $_POST['quote'];
+    
+    echo '<strong>Original Quote</strong';
+    
+    echo '<p>$quote</p>'; 
+    echo '<p>Word Count:</p>';
+    echo 'str_wrd_count("$quote")';
+    
+    echo '<strong>Quoted Words In Alphabetical Order:</strong>';
+    sort ($quote, SORT_NATURAL);
+        foreach ($quote as $key => $val) {
+            echo "quote [" . $key . "] = " . $val . "\n";
+        }
+    
+    //$quoteExplode = explode(' ', $quote);
     
     
     
-    echo 'Text Explode';
-    print_r($textExplode);
+   // echo 'Text Explode';
+    //print_r($textExplode);
     
-    $shuffledText = shuffle($textExplode);
+    //$shuffledText = shuffle($textExplode);
     
-    echo 'Shuffled Text';
-    print_r($textExplode);
+    //echo 'Shuffled Text';
+   // print_r($textExplode);
     
     //foreach loop
-    foreach($textExplode as $key => $value) {
-        echo "<p> $key Value: $value</p>";
-    }
+    //foreach($textExplode as $key => $value) {
+    //    echo "<p> $key Value: $value</p>";
+    //}
     
     
-    $textImplode = implode(' ', $textExplode);
-    echo"<p>$textImplode</p>";
+    //$textImplode = implode(' ', $textExplode);
+    //echo"<p>$textImplode</p>";
 
- for($i = 0; $i < $numParagraphs; $i++) {
-    echo "<p>$textImplode</p>";
- }
+ //for($i = 0; $i < $numParagraphs; $i++) {
+    //echo "<p>$textImplode</p>";
+ //}
  
 //     output a paragraph
 //     echo "<p>$text</p>"; 
@@ -35,3 +57,8 @@
 
 
 ?>
+
+
+
+   </body> 
+</html>
